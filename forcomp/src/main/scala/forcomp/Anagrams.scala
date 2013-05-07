ad1861d33428e9d19b1b7e009916acdff7b14312
Frank Hamand <fhamand@geneity.co.uk>
2013-05-07 15:09:07 +0100

    week 6 - wordOccurrences and sentenceOccurences

--- forcomp/src/main/scala/forcomp/Anagrams.scala
+++ forcomp/src/main/scala/forcomp/Anagrams.scala
@@ -33,10 +33,11 @@ object Anagrams {
    *  Note: the uppercase and lowercase version of the character are treated as the
    *  same character, and are represented as a lowercase character in the occurrence list.
    */
-  def wordOccurrences(w: Word): Occurrences = ???
-
+  def wordOccurrences(w: Word): Occurrences = {
+    w.groupBy((elem:Char)=>elem.toLower).map((mapping)=>(mapping._1, mapping._2.length))
+  }
   /** Converts a sentence into its character occurrence list. */
-  def sentenceOccurrences(s: Sentence): Occurrences = ???
+  def sentenceOccurrences(s: Sentence): Occurrences = wordOccurrences(s.mkString(''))
 
   /** The `dictionaryByOccurrences` is a `Map` from different occurrences to a sequence of all
    *  the words that have that occurrence count.
