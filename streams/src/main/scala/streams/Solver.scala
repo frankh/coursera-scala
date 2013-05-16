a757c2e6f0928401998c26b3f5fbbf67e78448ec
Frank <frankhamand@hotmail.com>
2013-05-16 14:03:05 +0200

    week 7 - final solution

--- streams/src/main/scala/streams/Solver.scala
+++ streams/src/main/scala/streams/Solver.scala
@@ -79,13 +79,16 @@ trait Solver extends GameDef {
    * The stream of all paths that begin at the starting block.
    */
   lazy val pathsFromStart: Stream[(Block, List[Move])] = 
-    from(startBlock #:: Stream(), Set())
+    from((startBlock, List[Move]()) #:: Stream[(Block, List[Move])](), Set())  
+  
   /**
    * Returns a stream of all possible pairs of the goal block along
    * with the history how it was reached.
    */
-  lazy val pathsToGoal: Stream[(Block, List[Move])] = pathsFromStart filter {
-    case (b, _) => b == goal
+  lazy val pathsToGoal: Stream[(Block, List[Move])] = {
+    pathsFromStart filter {
+      case (b, _) => b.b1 == goal && b.b2 == goal
+    }
   }
 
   /**
@@ -96,5 +99,5 @@ trait Solver extends GameDef {
    * the first move that the player should perform from the starting
    * position.
    */
-  lazy val solution: List[Move] = pathsToGoal.head
+  lazy val solution: List[Move] = pathsToGoal.head._2
 }
